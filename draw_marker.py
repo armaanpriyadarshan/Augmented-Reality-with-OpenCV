5208cbf6ab27cb8590aad4a0af5f2d49be71cda1
Armaan Priyadarshan <43357808+armaanpriyadarshan@users.noreply.github.com>
2021-04-25 13:52:02 -0400

    Update draw_marker.py

--- draw_marker.py
+++ draw_marker.py
@@ -4,6 +4,6 @@ import numpy as np
 dictionary = cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_250)
 
 markerImage = np.zeros((200, 200), dtype=np.uint8)
-markerImage = cv2.aruco.drawMarker(dictionary, 4, 200, markerImage, 1)
+markerImage = cv2.aruco.drawMarker(dictionary, 1, 200, markerImage, 1)
 
-cv2.imwrite("arucomarker4.png", markerImage)
+cv2.imwrite("arucomarker.png", markerImage)
